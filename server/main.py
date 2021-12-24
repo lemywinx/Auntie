9cfc2dccb57086d5a85d2394fd4e5f95279e85dc
Glenn Hope <glenn.alexander.hope@gmail.com>
2021-12-24 02:44:40 -0500

    Add try

--- server/main.py
+++ server/main.py
@@ -21,25 +21,26 @@ GAME_BOARD = [
 player_position = [0, 0]
 
 async def handle_echo(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
-    parsed = json.loads(message)
+    try:
+        data = await reader.read(100)
+        message = data.decode()
+        parsed = json.loads(message)
 
-    #addr = writer.get_extra_info('peername')
-    #print(parsed)
+        #addr = writer.get_extra_info('peername')
+        #print(parsed)
 
-    x = parsed["command"]["move"]
-    GAME_BOARD[player_position[1]][player_position[0]] = ' '
-    player_position[0] += x[0]
-    player_position[1] += x[1]
-    GAME_BOARD[player_position[1]][player_position[0]] = '@'
+        x = parsed["command"]["move"]
+        GAME_BOARD[player_position[1]][player_position[0]] = '_'
+        player_position[0] += x[0]
+        player_position[1] += x[1]
+        GAME_BOARD[player_position[1]][player_position[0]] = '@'
 
-    print("\n".join(["".join(row) for row in GAME_BOARD]))
+        print("\n".join(["".join(row) for row in GAME_BOARD]))
 
-    #writer.write(data)
-    #await writer.drain()
+        #writer.write(data)
+        #await writer.drain()
 
-    writer.close()
+        writer.close()
 
 async def main():
     server = await asyncio.start_server(handle_echo, '0.0.0.0', 26969)
